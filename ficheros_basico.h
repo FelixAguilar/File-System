9b613cb08b3b0c0db317fa2bc49b0b2bd0fe869d
AlvaroBueno99 <55491432+AlvaroBueno99@users.noreply.github.com>
2020-02-27 16:04:18 +0100

    Actualizado ficheros_basico.h

--- ficheros_basico.h
+++ ficheros_basico.h
@@ -41,35 +41,36 @@ struct superbloque
     char padding[BLOCKSIZE - 12 * sizeof(unsigned int)];
 };
 
-typedef union _inodo {
-    struct inodo
-    {
-        char tipo;     // Tipo ('l':libre, 'd':directorio o 'f':fichero)
-        char permisos; // Permisos (lectura y/o escritura y/o ejecución)
+struct inodo {    // Comprobar que ocupa 128 bytes haciendo un sizeof(inodo)!!!
+   char tipo;     // Tipo ('l':libre, 'd':directorio o 'f':fichero)
+   char permisos; // Permisos (lectura y/o escritura y/o ejecución)
+   
+   /* Por cuestiones internas de alineación de estructuras, si se está utilizando
+    un tamaño de palabra de 4 bytes (microprocesadores de 32 bits):
+   unsigned char reservado_alineacion1 [2];
+   en caso de que la palabra utilizada sea del tamaño de 8 bytes
+   (microprocesadores de 64 bits): unsigned char reservado_alineacion1 [6]; */
 
-        /* 
-        Por cuestiones internas de alineación de estructuras, si se está 
-        utilizando un tamaño de palabra de 4 bytes (microprocesadores de 32 
-        bits): unsigned char reservado_alineacion1 [2]; en caso de que la 
-        palabra utilizada sea del tamaño de 8 bytes (microprocesadores de 64 
-        bits): unsigned char reservado_alineacion1 [6]; 
-        */
+   char reservado_alineacion1[6];
+   time_t atime; // Fecha y hora del último acceso a datos: atime
+   time_t mtime; // Fecha y hora de la última modificación de datos: mtime
+   time_t ctime; // Fecha y hora de la última modificación del inodo: ctime
 
-        char reservado_alineacion1[6];
-        time_t atime; // Fecha y hora del último acceso a datos: atime
-        time_t mtime; // Fecha y hora de la última modificación de datos: mtime
-        time_t ctime; // Fecha y hora de la última modificación del inodo: ctim
+   /* Comprobar el tamaño del tipo time_t para vuestra plataforma/compilador:*/
 
-        unsigned int nlinks; // Cantidad de enlaces de entradas en directorio
-        unsigned int tamEnBytesLog; // Tamaño en bytes lógicos.
-        unsigned int numBloquesOcupados; // Bloques ocupados zona de datos
+   unsigned int nlinks;             // Cantidad de enlaces de entradas en directorio
+   unsigned int tamEnBytesLog;      // Tamaño en bytes lógicos. Se actualizará al escribir si crece
+   unsigned int numBloquesOcupados; // Cantidad de bloques ocupados zona de datos
 
-        unsigned int punterosDirectos[12];  // 12 punteros a bloques directos
-        unsigned int punterosIndirectos[3]; /* 3 punteros a bloques indirectos:
-        time_t 1 indirecto simple, 1 indirecto doble, 1 indirecto triple */
-    };
-    char padding[INODOSIZE];
-} inodo_t;
+   unsigned int punterosDirectos[12];  // 12 punteros a bloques directos
+   unsigned int punterosIndirectos[3]; /* 3 punteros a bloques indirectos:
+   1 indirecto simple, 1 indirecto doble, 1 indirecto triple */
+
+   /* Utilizar una variable de alineación si es necesario para vuestra plataforma/compilador */
+   char padding[INODOSIZE - 2 * sizeof(unsigned char) - 3 * sizeof(time_t) - 18
+    * sizeof(unsigned int) - 6 * sizeof(unsigned char)];
+};
+
 
 int tamMB(unsigned int nbloques);
 int tamAI(unsigned int nbloques);
