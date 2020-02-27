53eba99ecde6e0e65b03dcc034baafe23cc7541d
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2020-02-27 11:22:23 +0100

    Añadido funcion tamMB
    - Añadida la funcion tamMB con comentario incluido.

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -1,1 +1,24 @@
 #include "ficheros_basico.h"
+
+/* Funcion: tamMB
+* --------------
+* Devuelve el tamaño del espacio de disco ocupado por el mapa de bits en 
+* bloques segun el numero de bloques en el disco.
+*
+*   nbloques: numero de bloques en el disco.
+*
+*   returns: tamaño del mapa en bloques.
+*/
+int tamMB(unsigned int nbloques)
+{
+    int bits = nbloques / 8;
+    int bloques = bits / BLOCKSIZE;
+    if (bits % BLOCKSIZE)
+    {
+        return bloques + 1;
+    }
+    else
+    {
+        return bloques;
+    }
+}
