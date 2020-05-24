f4584245a204f147e76d63547982526f13ec5892
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2020-05-24 20:41:49 +0200

    Add files via upload

--- simulacion.c
+++ simulacion.c
@@ -1,20 +1,19 @@
-// Félix Aguilar, Adrián Bennasar, Álvaro Bueno
+// Autores: Félix Aguilar, Adrián Bennasar, Álvaro Bueno
 #include "simulacion.h"
 
 int acabados = 0;
 
 /* Fichero: simulacion.c:
-* ----------------------
-* 
+* -----------------------
+* Ejecuta simultaneamte escrituras en el sistema utilizando los hilos.
 *
 *  argc: número de argumentos introducidos.
 *  argv: char array de los argumentos introducidos.
 *
-*  returns: 
 */
 int main(int argc, char *argv[])
 {
-    // Variables.
+    // Variables utilizadas en la simulacion.
     char *disco;
     char tiempo[80];
     char camino[150];
@@ -42,32 +41,37 @@ int main(int argc, char *argv[])
     // Obtencion de la fecha y hora para el path de la raiz.
     t = time(NULL);
     strftime(tiempo, sizeof(tiempo), "%Y%m%d%H%M%S", localtime(&t));
-    strcat(camino, "/simul_");
-    strcat(camino, tiempo);
-    strcat(camino, "/");
+    sprintf(camino, "/simul_%s/", tiempo);
 
+    // Crea el directorio para la simulación en la raiz.
     if ((error = mi_creat(camino, 6)) < 0)
     {
         mostrar_error_directorios(error);
         bumount();
         return EXIT_FAILURE;
     }
+    printf("Directorio simulacion: %s\n", camino);
+
+    // Asigna la señal de finalizacion de hijo con el reaper.
     signal(SIGCHLD, reaper);
 
-    printf("Directorio simulacion: %s\n", camino);
+    // Entra en el bucle para la creacion de todos los hijos.
     proceso = 0;
     while (proceso < PROCESOS)
     {
+        // Crea un nuevo hijo.
         pid = fork();
+
+        // Si es un hijo.
         if (!pid)
         {
-            // Monta el disco para el hijo.
+            // Monta el disco.
             if (bmount(disco) < 0)
             {
                 fprintf(stderr, "Error: No se ha podido montar el disco.\n");
                 exit(0);
             }
-            // Crea el directorio del hijo.
+            // Crea el directorio.
             sprintf(camino_hijo, "%sproceso_%d/", camino, getpid());
             if ((error = mi_creat(camino_hijo, 6)) < 0)
             {
@@ -76,7 +80,7 @@ int main(int argc, char *argv[])
                 bumount();
                 exit(0);
             }
-            // Crea el archivo del hijo.
+            // Crea el archivo.
             sprintf(camino_hijo, "%sprueba.dat", camino_hijo);
             if ((error = mi_creat(camino_hijo, 6)) < 0)
             {
@@ -90,10 +94,13 @@ int main(int argc, char *argv[])
             operacion = 0;
             while (operacion < OPERACIONES)
             {
+                // Introduce los valores del registro.
                 registro.fecha = time(NULL);
                 registro.pid = getpid();
                 registro.nEscritura = operacion + 1;
                 registro.nRegistro = rand() % REGMAX;
+
+                // Escribe el registro en su posicion asignada.
                 if ((error = mi_write(camino_hijo, &registro,
                                       registro.nRegistro * sizeof(struct registro),
                                       sizeof(struct registro))) < 0)
@@ -102,30 +109,35 @@ int main(int argc, char *argv[])
                     bumount();
                     exit(0);
                 }
+                // Augmenta el contador y espera para la siguiente operacion.
                 operacion++;
                 usleep(WAITH);
             }
-
+            // Imprime el mensaje de finalizacion y desmonta el disco.
             printf("Proceso %d: Completadas %d escrituras en %s\n", proceso + 1,
                    operacion, camino_hijo);
-            // Desmonta el disco.
             bumount();
             exit(0);
         }
+        // Augmenta el contador y espera para crear el siguiente hijo.
         proceso++;
         usleep(WAITP);
     }
-
+    // Mientras no hayan acabado todos, el padre espera.
     while (acabados < PROCESOS)
     {
         pause();
     }
+    // Desmonta el disco y finaliza su ejecucion.
     bumount();
-
     printf("Total procesos terminados: %d\n", proceso);
     return EXIT_SUCCESS;
 }
 
+/* Funcion: reaper:
+* -----------------
+* Funcion la cual se le llama cuando un hijo finaliza su ejecucion.
+*/
 void reaper()
 {
     pid_t ended;
@@ -133,6 +145,8 @@ void reaper()
     while ((ended = waitpid(-1, NULL, WNOHANG)) > 0)
     {
         acabados++;
-        //fprintf(stderr, "acabado %d total acabados: %d\n", ended, acabados);
+#if DEBUG
+        fprintf(stderr, "acabado %d total acabados: %d\n", ended, acabados);
+#endif
     }
 }
--- simulacion.h
+++ simulacion.h
@@ -1,20 +1,21 @@
+// Autores: Félix Aguilar, Adrián Bennasar, Álvaro Bueno
 #include <sys/wait.h>
 #include <signal.h>
 #include "directorios.h"
 
-#define OPERACIONES 50
-#define PROCESOS 100
+#define OPERACIONES 50 // Operaciones a realizar por cada hijo.
+#define PROCESOS 100   // Hilos de ejecucion que se van a realizar.
 
-#define REGMAX 500000
-#define WAITP 200000
-#define WAITH 50000
+#define REGMAX 500000 // Registro maximo a escribir.
+#define WAITP 200000  // Tiempo de espera del padre (microseg)
+#define WAITH 50000   // Tiempo de espera del hijo (microseg)
 
 struct registro
 {
-    time_t fecha;   //fecha de la escritura en formato epoch
-    pid_t pid;      //PID del proceso que lo ha creado
-    int nEscritura; //Entero con el número de escritura (de 1 a 50)
-    int nRegistro;  //Entero con el número del registro dentro del fichero (de 0 a REGMAX-1)
+    time_t fecha;   // Fecha de la escritura en formato epoch.
+    pid_t pid;      // PID del proceso que lo ha creado.
+    int nEscritura; // Entero de escritura (de 1 a 50).
+    int nRegistro;  // Entero del registro dentro del fichero (de 0 a REGMAX-1).
 };
 
 void reaper();
